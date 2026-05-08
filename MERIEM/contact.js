3ecf0a316eaba93d51b015b49387f48db8f5f1cb
Meriem ER.RAHMOUNI <meriemerrahmouni06@gmail.com>
2026-05-08 19:56:34 +0100

    modification de page contact

--- MERIEM/contact.js
+++ MERIEM/contact.js
@@ -1,7 +1,9 @@
+
+const form =document.getElementById('contact-form');
 const btn = document.getElementById('btn-envoyer'); // On utilise l'ID ici
 const bar = document.getElementById('success-bar');
-
-btn.addEventListener('click', (e) => {
+if(form){ 
+form.addEventListener('submit', (e) => {
     e.preventDefault(); 
     console.log("Clic détecté !"); // Ceci s'affichera dans ta console (F12) pour tester
     bar.classList.add('show');
@@ -9,4 +11,6 @@ btn.addEventListener('click', (e) => {
     setTimeout(() => {
         bar.classList.remove('show');
     }, 4000);
+    form.reset();
 });
+}
